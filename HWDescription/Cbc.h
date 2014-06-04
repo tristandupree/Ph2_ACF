7b024ca00f516b5c9e52992b81904f7182cf950b
xtaldaq <georg.auzinger@cern.ch>
2014-06-04 13:41:33 +0200

    adding a struct CbcRegItem in Namespace HWDescription drafting some members & methods for Cbc Class

--- HWDescription/Cbc.h
+++ HWDescription/Cbc.h
@@ -2,16 +2,20 @@
 #define Cbc_h__
 
 #include "HWDescription/FEDescription.h"
+#include "HWDescription/CbcRegItem.h"
 
 // Cbc2 Chip HW Description Class
 
 namespace HWDescription{
 
+	class CbcRegItem;
+
+	typedef std::map< std::string, CbcRegItem > CbcRegMap; 
+
 	class Cbc : public FEDescription{
 
 	public:
 
-		
 		// C'tors with object FE Description 
 		Cbc( FEDescription pFeDesc, UInt_t pCbcId, std::string filename );
 		Cbc( FEDescription pFeDesc, UInt_t pCbcId, /*most important parameters*/ );
@@ -28,10 +32,51 @@ namespace HWDescription{
 		// D'tor
 		~Cbc();
 
+		UInt_t getTriggerLatency( return fTriggerLatency );
+		UInt_T setTriggerLatency();
+
+		UInt_t getVcth( return fVcth );
+		UInt_t setVcth();
+
+		void dumpRegValues( std::string filename );
+		void updateRegValues( std::string filename );
+
+	protected:
+
+		// Map of Register Name vs. RegisterItem that contains: Page, Address, Default Value, Value
+		CbcRegMap fRegMap;
 
 	private:
 
-	protected:
+		// CBC Register values, all in hex representation
+		// think of a better (more lightweight datatype for hex representation of register values: 8 bit each, range from 0 to 255)
+		UInt_t fFrontEndControl;                              	
+		UInt_t fTriggerLatency;                               	
+		UInt_t fHitDetectSLVS ;                               	
+		UInt_t fIpre1;                                       	
+		UInt_t fIpre2;                                        	
+		UInt_t fIpsf;                                         	
+		UInt_t fIpa;                                          	
+		UInt_t fIpaos;                                        	
+		UInt_t fVpafb;                                        	
+		UInt_t fIcomp;                                        	
+		UInt_t fVpc;                                          	
+		UInt_t fVplus;                                        	
+		UInt_t fVCth;                                         	
+		UInt_t fTestPulsePot;   
+		// Reg Name is SelTestPulseDel&ChanGroup                   	
+		UInt_t fSelTestPulseDelChanGroup;                    	
+		// Reg Name is MiscTestPulseCtrl&AnalogMux 
+		UInt_t fMiscTestPulseCtrlAnalogMux;                  	
+		UInt_t fTestPulseChargePumpCurrent;                   	
+		UInt_t fTestPulseChargeMirrCascodeVolt;               	
+		// Reg Name is CwdWindow&Coincid
+		UInt_t fCwdWindowCoincid;                            	
+		UInt_t fMiscStubLogic;
+		// Mask channels in Groups of 8
+		bool   fMaskChanGroup[33];
+		// Channel Offsets
+		UInt_t fChannelOffset[254];    
 
 	};
 }
---
+++ HWDescription/CbcRegItem.h
@@ -0,0 +1,16 @@
+#ifndef _CbcRegItem_h__
+#define _CbcRegItem_h__
+
+namespace HWDescription{
+
+	struct CbcRegItem{
+
+		UInt_t fPage;
+		UInt_t fAddress;
+		UInt_t fDefValue;
+		UInt_t fValue;
+		
+	};
+}
+
+#endif
