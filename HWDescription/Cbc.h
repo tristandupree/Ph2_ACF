30fac28e0991712f1ff8f0cf9b4fb6b28b5c35d7
xtaldaq <georg.auzinger@cern.ch>
2014-06-04 12:30:59 +0200

    making Cbc Class inherit from FEDescription

---
+++ HWDescription/Cbc.h
@@ -0,0 +1,21 @@
+#ifndef Cbc_h__
+#define Cbc_h__
+
+#include "HWDescription/FEDescription.h"
+
+// Cbc2 Chip HW Description Class
+
+namespace HWDescription{
+
+	class Cbc : public FEDescription{
+	public:
+
+	private:
+
+	protected:
+		
+	};
+}
+
+
+#endif
