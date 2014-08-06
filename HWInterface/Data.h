aed17a2fad8db126aca7bf00e7867b25ca8febb3
nipierre <nico@tribupierre.net>
2014-08-06 21:59:19 +0200

    Update Data.h

--- HWInterface/Data.h
+++ HWInterface/Data.h
@@ -25,32 +25,82 @@ namespace Ph2_HwInterface
 
     /*!
     * \class Data
-    * \brief Dummy data format in order to test
+    * \brief Data container to manipulate data flux from the Cbc
     */
     class Data
     {
         private:
-            char *fBuf;
-            uint32_t fBufSize;
-            uint32_t fNevents;
-            Event fEvent;
-            uint32_t fCurrentEvent;
+            char *fBuf; /*! Data buffer <*/
+            uint32_t fBufSize; /*! Size of Data buffer <*/
+            uint32_t fNevents; /*! Number of Events<*/
+            Event fEvent; /*! Events container <*/
+            uint32_t fCurrentEvent; /*! Current Event in use <*/
 
         private:
+            /*!
+            * \brief Byte swapping (not used)
+            * \param *org : input byte string
+            * \param *swapped : output byte string
+            * \param nbytes : lenght of byte string
+            */
             void swapByteOrder( const char *org, char *swapped, unsigned int nbyte );
 
         public:
+            /*!
+            * \brief Constructor of the Data class
+            * \param pNbCbc
+            */
             Data(uint32_t pNbCbc):fBuf(0),fCurrentEvent(0),fEvent(pNbCbc) {}
+            /*!
+            * \brief Constructor of the Data class
+            * \param pBoard : Board to work with
+            * \param pNbCbc
+            */
             Data(BeBoard& pBoard, uint32_t pNbCbc);
+            /*!
+            * \brief Copy Constructor of the Data class
+            */
             Data(Data &pData);
+            /*!
+            * \brief Destructor of the Data class
+            */
             ~Data() {if(fBuf) free(fBuf);}
 
+            /*!
+            * \brief Initialise the data structure
+            * \param pNevents : number of Events
+            */
             void Initialise( uint32_t pNevents );
+            /*!
+            * \brief Initialise the data structure
+            * \param pNevents : number of Events
+            * \param pBoard : Board to work with
+            */
             void Initialise( uint32_t pNevents, BeBoard& pBoard );
+            /*!
+            * \brief Set the data in the data map
+            * \param *pData : Data from the Cbc
+            */
             void Set( void *pData );
+            /*!
+            * \brief Reset the data structure
+            */
             void Reset();
+            /*!
+            * \brief Copy the data buffer
+            * \param pData : Data to copy in
+            */
             void CopyBuffer( Data &pData );
+            /*!
+            * \brief Copy the data buffer
+            * \param pBufSize : size of the buffer
+            * \return Data buffer
+            */
             const char * GetBuffer( uint32_t &pBufSize ) const;
+            /*!
+            * \brief Get the next Event
+            * \return Next Event
+            */
             const Event * GetNextEvent();
 
     };
