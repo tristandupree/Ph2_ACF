4bd845e501aee58ab17a8aba17b935acb2e13e5c
Lorenzo Bidegain <lorenzo.bidegain@gmail.com>
2014-08-11 17:37:13 +0200

    Adding Shelve class

---
+++ HWDescription/Shelve.cc
@@ -0,0 +1,70 @@
+/*!
+*
+* \file Shelve.cc
+* \brief Shelve Description class, handles a vector of Board
+* \author Lorenzo BIDEGAIN
+* \date 11/08/14
+*
+* Support : mail to : lorenzo.bidegain@cern.ch
+*
+*/
+
+#include "Shelve.h"
+
+namespace Ph2_HwDescription{
+
+	Shelve::Shelve( uint8_t pShelveId)
+	{
+		fShelveId=pShelveId;
+	}
+
+	Shelve::Shelve()
+	{
+		fShelveId=0;
+	}
+
+
+	void Shelve::addBoard( BeBoard& pBoard )
+	{
+		fBoardVector.push_back( pBoard );
+	}
+
+
+	bool Shelve::removeBoard( uint8_t pBeId )
+	{
+		std::vector < BeBoard > :: iterator i;
+		bool j=false;
+		for (i=fBoardVector.begin();i!=fBoardVector.end();++i)
+		{
+			if (i->fBeId==pBeId)
+			{
+				fBoardVector.erase(i);
+				j=true;
+				i--;   //erase reduces the container size by the number of elements removed, which are destroyed. To avoid that the iterator point an unallocated part of the memory, we need to decrease the iterator
+			}
+		}
+		if (j==true)
+		return true;
+		else
+		{
+			std::cout<<"Error:This Shelve doesn't have the Board"<<std::endl;
+			return false;
+		}
+	}
+
+
+	BeBoard*   Shelve::getBoard( uint8_t pBeId )
+	{
+		std::vector < BeBoard > :: iterator i;
+		for (i=fBoardVector.begin();i!=fBoardVector.end();++i)
+		{
+			if (i->fBeId==pBeId)
+				{
+					return &*i ;
+				}
+		}
+		return NULL;
+
+	}
+
+}
