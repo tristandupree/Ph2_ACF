6c8123a48c85fedc865b961d66f4ad913d2cd453
G. Auzinger <georg.auzinger@cern.ch>
2014-08-19 17:03:58 +0200

    bugfix after merging

--- HWDescription/BeBoard.h
+++ HWDescription/BeBoard.h
@@ -134,7 +134,6 @@ namespace Ph2_HwDescription{
 		uint8_t fShelveId;
 		uint8_t fBeId;
 
-		std::vector< Module > fModuleVector; /*!< Vector of FEModules, each module is supposed to know which FMC slot it is connected to... */
 
 		BeBoardRegMap fRegMap; /*!< Map of BeBoard Register Names vs. Register Values */
 
